443eb5fd824a031500b50fee1accaa75b7491cd1
chgian <chgian77@gmail.com>
2021-12-30 21:41:14 +0000

    suckercoin

--- CryptoSucker/CryptoSucker/ClientApp/src/Containers/Buy/Buy.tsx
+++ CryptoSucker/CryptoSucker/ClientApp/src/Containers/Buy/Buy.tsx
@@ -3,9 +3,13 @@
 
 export default function Buy() {
 
+    //const ethereum = window.ethereum;
+
     return (
         <div>
             <h1>Buy sucker coins, sucker!</h1>
+            <h1>Buy sucker coins, sucker!</h1>
+
         </div>
     );
 }
--- CryptoSucker/CryptoSucker/ClientApp/src/Containers/Home/Home.tsx
+++ CryptoSucker/CryptoSucker/ClientApp/src/Containers/Home/Home.tsx
@@ -1,11 +1,16 @@
 import * as React from 'react';
 import { Link } from 'react-router-dom';
+import './Home.css';
 
 export default function Home() {
     return (
-        <div>
-            <h1>Hello, Sucker!</h1>
-            <p>Do you want to buy Sucker coin? </p>
+        <div className="home-main">
+            <h1>Do you spend all day trying to find the best crypto coins?</h1>
+            <h1>Is your portfolio full of shit coins, waiting for them to go 1000x?</h1>
+            <h1>Do your close people consider you as a  </h1>
+            <h1>CRYPTO SUCKER? </h1>
+            <h1>Don't get dissapointed. Buy the coin specifically for crypto suckers!</h1>
+
             <Link to="/buy">Click here</Link>
         </div>
     );
